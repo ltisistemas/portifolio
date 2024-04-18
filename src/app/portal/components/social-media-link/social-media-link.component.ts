c49340601a7e31af8fa195d004f52ceaa34b0cc5
Luiz Felipe Marinho Dantas <ltisistemasesolucoes@gmail.com>
2024-04-18 13:49:31 -0300

    Setup Social Midia Link

---
+++ src/app/portal/components/social-media-link/social-media-link.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-media-link',
+  templateUrl: './social-media-link.component.html',
+  styleUrls: ['./social-media-link.component.scss'],
+})
+export class SocialMediaLinkComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
