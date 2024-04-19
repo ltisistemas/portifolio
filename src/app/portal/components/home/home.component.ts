d2d9cc260bafda2860ce911afb009f564592bfd4
Luiz Felipe Marinho Dantas <ltisistemasesolucoes@gmail.com>
2024-04-19 07:43:33 -0300

    Screen

--- src/app/portal/components/home/home.component.ts
+++ src/app/portal/components/home/home.component.ts
@@ -20,34 +20,5 @@ export class HomeComponent implements OnInit {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-
-    // https://drive.google.com/file/d/1icP9SG420TyvP6jEumVIEelPzhpq2eb1/view?usp=sharing
-
-    // const url =
-    //   'https://drive.google.com/file/d/1icP9SG420TyvP6jEumVIEelPzhpq2eb1/view';
-
-    // let headers = new HttpHeaders();
-    // headers = headers.set('Accept', 'application/pdf');
-    // this.http.get(url, { headers, responseType: 'blob' }).subscribe({
-    //   next: (response) => {
-    //     const blob = new Blob([response], {
-    //       type: 'applicationapplication/pdf',
-    //     });
-
-    //     console.log('> Response', response);
-    //   },
-    // });
-
-    // firstValueFrom(request).then((response) => {
-    //   const blob = new Blob([response], { type: 'applicationapplication/pdf' });
-    //   const url = window.URL.createObjectURL(blob);
-    //   const link = document.createElement('a');
-    //   link.setAttribute('href', url);
-    //   link.setAttribute('download', 'Resume Europass Luiz Felipe Marinho.pdf');
-    //   link.style.display = 'none';
-    //   document.body.appendChild(link);
-    //   link.click();
-    //   document.body.removeChild(link);
-    // });
   }
 }
